948d26c5136823f468ff7c9f82cde4ed24c5b46a
anriadamia <anriadamia2000@gmail.com>
2022-11-22 18:34:29 +0400

    Final Project

--- burgerbar.js
+++ burgerbar.js
@@ -1,7 +1,9 @@
 let toggleBurgerbar=document.getElementById("burgerbar-container");
 let navigationToggle=document.getElementById("navigation");
+let main=document.querySelector("main");
 
 toggleBurgerbar.addEventListener("click",()=>{
   toggleBurgerbar.classList.toggle("active");
   navigationToggle.classList.toggle("navactive");
+ main.classList.toggle("disable");
 })
