127d996d7385bc8eb5439fbf5096d2e30ba27a1a
anriadamia <anriadamia2000@gmail.com>
2022-11-23 14:42:31 +0400

    Final Project

---
+++ accordion.js
@@ -0,0 +1,7 @@
+const accordion=document.querySelectorAll(".accordionContainer");
+
+accordion.forEach(item=>{
+    item.addEventListener("click",function(){
+        this.classList.toggle("active");
+    })
+})
---
+++ backtotop.js
@@ -0,0 +1,7 @@
+let backtoTop=document.getElementById("backtoTop");
+backtoTop.addEventListener("click",function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
